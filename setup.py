b3b6058e981d1dd7d574df9799db1829e29148c3
mariomorvan <mario.morvan.18@ucl.ac.uk>
2020-09-16 11:34:02 +0100

    update url

--- setup.py
+++ setup.py
@@ -6,19 +6,19 @@ with open('README.md', 'r') as dh:
 setuptools.setup(
     name="pylightcurve-torch",
     version_config=True,
-    setup_requires=['setuptools-git-versioning'],
     author="Mario Morvan",
     author_email="mario.morvan.18@ucl.ac.uk",
+    url="https://github.com/ucl-exoplanets/pylightcurve-torch.git",
     description="Transit modelling in Pytorch",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/mariomorvan/pylightcurve-torch.git",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: OS Independent",
     ],
+    setup_requires=['setuptools-git-versioning'],
     python_requires='>=3.7',
     install_requires=['torch']
 )
